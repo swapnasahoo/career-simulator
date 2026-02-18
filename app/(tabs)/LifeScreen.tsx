fa35fa98c1b4137f691f433cd992bed091279bb2
swapnasahoo <sahooswapnaswarup@gmail.com>
2026-02-18 11:32:21 +0530

    Add basic screens and tabs layout

---
+++ app/(tabs)/LifeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const LifeScreen = () => {
+  return (
+    <View>
+      <Text>LifeScreen</Text>
+    </View>
+  )
+}
+
+export default LifeScreen
---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Tabs } from 'expo-router'
+import React from 'react'
+
+const TabsLayout = () => {
+  return (
+    <Tabs></Tabs>
+  )
+}
+
+export default TabsLayout
