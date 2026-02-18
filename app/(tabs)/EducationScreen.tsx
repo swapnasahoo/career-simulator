fd6a6c95e37337fdd1e2dc95771c04d09ccff306
swapnasahoo <sahooswapnaswarup@gmail.com>
2026-02-18 11:34:16 +0530

    Update TabsLayout and RootLayout to not show header

--- app/(tabs)/EducationScreen.tsx
+++ app/(tabs)/EducationScreen.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { Text, View } from "react-native";
 
 const EducationScreen = () => {
   return (
     <View>
       <Text>EducationScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default EducationScreen
+export default EducationScreen;
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,8 @@
-import { Tabs } from 'expo-router'
-import React from 'react'
+import { Tabs } from "expo-router";
+import React from "react";
 
 const TabsLayout = () => {
-  return (
-    <Tabs></Tabs>
-  )
-}
+  return <Tabs screenOptions={{ headerShown: false }}></Tabs>;
+};
 
-export default TabsLayout
+export default TabsLayout;
