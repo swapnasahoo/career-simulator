60d6ffb80450db75d94458e50eadf743b124829d
swapnasahoo <sahooswapnaswarup@gmail.com>
2026-02-18 11:41:21 +0530

    Update TabsLayout to define tabs structure and their styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,53 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import React from "react";
 
 const TabsLayout = () => {
-  return <Tabs screenOptions={{ headerShown: false }}></Tabs>;
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="SkillsScreen"
+        options={{
+          title: "Skills",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "construct" : "construct-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="LifeScreen"
+        options={{
+          title: "Life",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="EducationScreen"
+        options={{
+          title: "Education",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "school" : "school-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 };
 
 export default TabsLayout;
