3b9de0af8648c2978cc7a9be640ce0d2a732314f
swapnasahoo <sahooswapnaswarup@gmail.com>
2026-02-23 12:21:34 +0530

    Add skills screen header

--- app/(tabs)/SkillsScreen.tsx
+++ app/(tabs)/SkillsScreen.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SkillsScreen = () => {
   return (
-    <View>
-      <Text>SkillsScreen</Text>
+    <View className="flex-1 bg-bg px-6 py-4">
+      <SafeAreaView>
+        {/* HEADER */}
+        <Text className="text-text text-center text-lg font-semibold">
+          Skills
+        </Text>
+      </SafeAreaView>
     </View>
   );
 };
